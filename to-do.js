051abdac13c7f86caa1aad580771ed76bdf9dc96
Sohel khan <119399499+SOHELCHAUHAN@users.noreply.github.com>
2025-04-24 13:55:56 +0530

    Update to-do.js

--- to-do.js
+++ to-do.js
@@ -1,30 +1,12 @@
 const taskInput = document.getElementById("task-input");
 const tasklistContainer = document.getElementById("task-list-container");
 
-const dueDateInput = document.getElementById("task-due-date");
-const addTaskButton = document.getElementById("add-task-btn");
-const taskCheckbox = document.getElementById("checkbox-task");
+// const dueDateInput = document.getElementById("task-due-date");
+// const addTaskButton = document.getElementById("add-task-btn");
+// const taskCheckbox = document.getElementById("checkbox-task");
 const alltask = document.getElementsByClassName("all");
-const completedtask = document.getElementsByClassName("completed");
+const finished = document.getElementsByClassName("completed");
 const duetasks = document.getElementsByClassName("incomplete");
-// const darkModeToggle = document.getElementById("toggle-dark-mode");
-
-// darkModeToggle.addEventListener("click",function(clr){
-//  if (clr === "dark") document.body.classList.toggle("dark-mode");
-//  else if (clr === "light") document.body.classList.toggle("light-mode");}
-// total=()=>{
-// if 
-// }
-// completedtask=()=>{
-//   if (taskInput.value === "checked" ){
-//   console.log("${tasklistcontainer}"); 
-//     }else if ( taskInput.value != ""); 
-//   }
-
-
-//  pandingtasks=()=>{
-//   if ( ) 
-//   }
 
 
 
@@ -43,6 +25,7 @@ function addTask() {
   }
   taskInput.value ="";
   saveData();
+  loadData();
 }
 
 tasklistContainer.addEventListener("click",function(e){
@@ -52,7 +35,68 @@ tasklistContainer.addEventListener("click",function(e){
     }else if (e.target.tagName=="SPAN"){
         e.target.parentElement.remove();
     }saveData();
+    loadData();
 },false);
+  
+// const total=()=>
+  // document.querySelectorAll("task-list");
+  // console.log("li");
+
+// -----
+
+  const total = () => {
+    const tasks = document.querySelectorAll("#task-list-container li");
+    tasks.forEach(task => {
+      task.style.display = "flex";
+      // task.style.
+    });saveData();
+    console.log(" all tasks");
+  };
+  
+  const completedtask = () => {
+    const tasks = document.querySelectorAll("#task-list-container li");
+    tasks.forEach(task => {
+      if (task.classList.contains("checked")) {
+        task.style.display = "flex";
+      
+      } else {
+        task.style.display = "none";
+      }
+    }); saveData();
+    console.log("completed tasks");
+  };
+  
+  const pandingtasks = () => {
+    const tasks = document.querySelectorAll("#task-list-container li");
+    tasks.forEach(task => {
+      if (!task.classList.contains("checked")) {
+        task.style.display = "flex";
+      } else {
+        task.style.display = "none";
+      }
+    });saveData();
+    console.log(" incomplete tasks");
+  };
+
+//  const completedtask=()=>
+//   document.querySelectorAll("#task-list");
+//  tasklistContainer.forEach(("li") => {
+//   if ()
+//  });
+
+
+
+
+//  const task = tasklistContainer.getElementsByTagName("li");
+//  completedtask=()=>{
+//   if (tasklistContainer.classList.contains("checked"){
+//    console.log("")
+//     }else if ( taskInput.value.classList != "checked"){
+//       console.log("");
+//     }
+//     saveData();
+//   }
+
 
  saveData=()=>{
   localStorage.setItem('data', tasklistContainer.innerHTML);
@@ -67,4 +111,18 @@ const loadData = () => {
 window.onload = () => {
   loadData();
 };
+
+// const darkModeToggle = document.getElementById("toggle-dark-mode");
+
+// darkModeToggle.addEventListener("click",function(clr){
+//  if (clr === "dark") document.body.classList.toggle("dark-mode");
+//  else if (clr === "light") document.body.classList.toggle("light-mode");}
+// total=()=>{
+// if ()
+// }
+
+
+//  pandingtasks=()=>{
+//   if ( ) 
+//   }
 
